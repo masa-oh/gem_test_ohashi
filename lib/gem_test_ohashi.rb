0aed72b81738406803a9f7dc990cb7cec142c276
masa-oh <makun0084pc@gmail.com>
2022-02-12 11:40:06 +0900

    feat: 文字列に濁点をつけて返す機能とテストコードを追加

--- lib/gem_test_ohashi.rb
+++ lib/gem_test_ohashi.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative "gem_test_ohashi/version"
+require_relative "gem_test_ohashi/translator"
 
 module GemTestOhashi
   class Error < StandardError; end
--- spec/gem_test_ohashi_spec.rb
+++ spec/gem_test_ohashi_spec.rb
@@ -1,11 +1,7 @@
 # frozen_string_literal: true
 
 RSpec.describe GemTestOhashi do
-  it "has a version number" do
-    expect(GemTestOhashi::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "入力した文字に濁点をつけて返す" do
+    expect(GemTestOhashi::Translator.shout("どうしてなんだよぉおお！！うわぁぁあああああああああああ")).to eql("ど゛う゛し゛て゛な゛ん゛だ゛よ゛ぉ゛お゛お゛！゛！゛う゛わ゛ぁ゛ぁ゛あ゛あ゛あ゛あ゛あ゛あ゛あ゛あ゛あ゛あ゛あ゛")
   end
 end
