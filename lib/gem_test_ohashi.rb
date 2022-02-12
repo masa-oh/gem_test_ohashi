0acdcda1f79305e3677eeba42763187ebba13853
masa-oh <makun0084pc@gmail.com>
2022-02-12 11:03:38 +0900

    initial commit

---
+++ lib/gem_test_ohashi.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "gem_test_ohashi/version"
+
+module GemTestOhashi
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/gem_test_ohashi_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe GemTestOhashi do
+  it "has a version number" do
+    expect(GemTestOhashi::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
